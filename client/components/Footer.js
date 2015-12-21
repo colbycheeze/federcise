fbf1bb0100bd5608e3b4b9a567404830445f8f1f
Colby Williams <colby@colbycheeze.com>
2015-12-21 09:43:53 -0600

    pushing latest changes

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,7 +3,6 @@ Footer = React.createClass({
   render: function() {
     return (
       <footer>
-        
       </footer>
     );
   }
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -2,21 +2,34 @@ const {Link} = ReactRouter;
 
 Nav = React.createClass({
 
-  render: function() {
-    return (
-      <nav className="row">
-        <ul className="nav col row grow-xs">
-          <li><a className="btn" href="#">About</a></li>
-          <li><a className="btn active" href="#">Getting Started</a></li>
-          <li><Link to="/challenges" className="btn" activeState="active">Challenge Archive</Link></li>
-          <li><a className="btn" href="#">Resources</a></li>
-        </ul>
+render: function() {
+  return (
+    <nav className="row between container-fluid">
+      <ul className="nav col fit row">
+        <li><a className="btn" href="#">About</a></li>
+        <li><a className="btn active" href="#">Getting Started</a></li>
+        <li><Link to="/challenges" className="btn" activeState="active">Challenge Archive</Link></li>
+        <li><a className="btn" href="#">Resources</a></li>
+      </ul>
 
-        <ul className="col xs3 end-xs">
-          <li><a className="btn" href="#">Login with GitHub</a></li>
-        </ul>
-      </nav>
-    );
-  }
+      <ul className="col fit">
+        <li><a className="btn" href="#">Login with GitHub</a></li>
+      </ul>
+    </nav>
+
+    // <nav className="row between container-fluid">
+    //   <ul className="nav col fit row">
+    //     <li><a className="btn" href="#">About</a></li>
+    //     <li><a className="btn active" href="#">Getting Started</a></li>
+    //     <li><Link to="/challenges" className="btn" activeState="active">Challenge Archive</Link></li>
+    //     <li><a className="btn" href="#">Resources</a></li>
+    //   </ul>
+    //
+    //   <ul className="col fit">
+    //     <li><a className="btn" href="#">Login with GitHub</a></li>
+    //   </ul>
+    // </nav>
+  );
+}
 
 });
